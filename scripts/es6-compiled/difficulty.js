fc5e29c6a2ecc27b8257674a39be5b99cfe532f5
Justin Mariner <justin.mariner@hotmail.com>
2015-10-29 10:21:33 -0400

    began working on choice class to replace much of tileregistry

--- scripts/es6-compiled/difficulty.js
+++ scripts/es6-compiled/difficulty.js
@@ -1,5 +1,11 @@
 "use strict";
 
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 var DIFFICULTY_DATA = [null,
 
 //----------------------DIFFICULTY 1-----------------------------
@@ -61,15 +67,13 @@ var DIFFICULTY_DATA = [null,
 {
 	main: [{
 		type: "integer",
-		weight: 3,
-		limits: [1, 12] //25]
-	} /*,
-   {
-   type: "integer",
-   weight: 1,
-   limits: [1, 99]
-   }*/
-	],
+		weight: 10,
+		limits: [1, 25]
+	}, {
+		type: "integer",
+		weight: 1,
+		limits: [1, 99]
+	}],
 	choices: [{
 		type: "integer",
 		condition: "mainNumber <= 12", // because multiplication tables
@@ -92,5 +96,103 @@ var DIFFICULTY_DATA = [null,
 	main: [{ type: "power", baseLimits: [1, 12], powerLimits: [2, 2] }],
 	choice: [{ type: "power", baseLimits: [1, 12], powerLimits: [2, 2] }]
 }];
+
+var Choice = (function () {
+	function Choice(diff) {
+		var isMain = arguments.length <= 1 || arguments[1] === undefined ? false : arguments[1];
+
+		_classCallCheck(this, Choice);
+
+		this.which = isMain ? "main" : "choices";
+		this.diff = diff;
+		this.data = undefined;
+	}
+
+	_createClass(Choice, [{
+		key: "id",
+		get: function get() {
+			return [this.diff, this.which.charAt(0), DIFFICULTY_DATA[this.diff][this.which].indexOf(this.data)].join(".");
+		}
+	}]);
+
+	return Choice;
+})();
+
+function _genSingleChoiceTile(diff, group, mainNumber) {
+
+	mainNumber = +mainNumber;
+
+	var choice = TileRegistry.getRandomTileData(diff);
+	var choicesSoFar = group.choices;
+
+	var reRoll = function reRoll() {
+		return TileRegistry.getRandomTileData(diff);
+	};
+
+	//called before adding any more elements, preventing going over max
+	var hasMax = function hasMax(choices) {
+		// currently limited to one option with maxCount; need some sort of ID system to determine which choice is which
+		var count = 0;
+		var max = 0;
+		return choices.some(function (c) {
+			if (c.maxCount !== undefined) {
+				if (max === 0) max = c.maxCount;
+				if (max > 0 && max === ++count) return true;
+			}
+		});
+	};
+
+	var success = false;
+	while (!success && (choice.condition !== undefined || choice.maxCount !== undefined)) {
+		success = true;
+		while (choice.condition !== undefined && math.eval(choice.condition, { mainNumber: mainNumber }) === false) {
+			choice = reRoll();
+			success = false;
+		}
+		while (choice.maxCount !== undefined && hasMax(choicesSoFar)) {
+			choice = reRoll();
+			success = false;
+		}
+	}
+	return choice;
+}
+
+function getRandomTileData(difficulty) {
+	var _Utils, _Utils2, _Utils3;
+
+	var isMain = arguments.length <= 1 || arguments[1] === undefined ? false : arguments[1];
+
+	var choices = DIFFICULTY_DATA[difficulty][isMain ? "main" : "choices"];
+
+	// get the random choice from the diff data
+	var choice = Utils.pickWeightedRandom(choices);
+
+	// compute the value using given limits and type
+	var value = undefined;
+	switch (choice.type) {
+		case "integer":
+			value = (_Utils = Utils).rand.apply(_Utils, _toConsumableArray(choice.limits));
+			break;
+		case "fraction":
+			// TODO do fraction stuff
+			break;
+		case "power":
+		case "exponent":
+			value = (_Utils2 = Utils).rand.apply(_Utils2, _toConsumableArray(choice.baseLimits)) + " ^ " + (choice.power || (_Utils3 = Utils).rand.apply(_Utils3, _toConsumableArray(choice.powerLimits)));
+			break;
+		default:
+			value = NaN;
+	}
+
+	var operation = isMain ? "" : choice.operation;
+
+	return {
+		value: value,
+		valueString: operation + value,
+		operation: operation,
+		condition: choice.condition,
+		maxCount: choice.maxCount
+	};
+}
 
 //# sourceMappingURL=difficulty.js.map
--- scripts/es6/difficulty.js
+++ scripts/es6/difficulty.js
@@ -74,14 +74,14 @@ var DIFFICULTY_DATA = [null,
 		main: [
 			{
 				type: "integer",
-				weight: 3,
-				limits: [1, 12]//25]
-			}/*,
+				weight: 10,
+				limits: [1, 25]
+			},
 			{
 				type: "integer",
 				weight: 1,
 				limits: [1, 99]
-			}*/
+			}
 		],
 		choices: [
 			{
@@ -115,13 +115,87 @@ var DIFFICULTY_DATA = [null,
 
 class Choice {
     constructor(diff, isMain=false) {
-        var which = isMain ? "main" : "choices";
-        var data =
-Utils.getWeightedRandom(DIFFICULTY_DATA[diff][which];
-        this.id = [
-            diff,
-            which.charAt(0),
-            DIFFICULTY_DATA[diff][which].indexOf(data)
-        ].join(".");
+        this.which = isMain ? "main" : "choices";
+        this.diff = diff;
+		this.data = undefined;
     }
+
+	get id() {
+		return [
+			this.diff,
+			this.which.charAt(0),
+			DIFFICULTY_DATA[this.diff][this.which].indexOf(this.data)
+		].join(".");
+	}
+}
+
+function _genSingleChoiceTile(diff, group, mainNumber) {
+
+	mainNumber = +mainNumber;
+
+	var choice = TileRegistry.getRandomTileData(diff);
+	var choicesSoFar = group.choices;
+
+	var reRoll = () => TileRegistry.getRandomTileData(diff);
+
+	//called before adding any more elements, preventing going over max
+	var hasMax = function(choices) { // currently limited to one option with maxCount; need some sort of ID system to determine which choice is which
+		var count = 0;
+		var max = 0;
+		return choices.some(function(c){
+			if (c.maxCount !== undefined) {
+				if (max === 0) max = c.maxCount;
+				if (max > 0 && max === ++count) return true;
+			}
+		});
+	};
+
+	var success = false;
+	while (!success && ((choice.condition !== undefined) || (choice.maxCount !== undefined))) {
+		success = true;
+		while (choice.condition !== undefined && math.eval(choice.condition, {mainNumber}) === false) {
+			choice = reRoll();
+			success = false;
+		}
+		while (choice.maxCount !== undefined && hasMax(choicesSoFar)) {
+			choice = reRoll();
+			success = false;
+		}
+	}
+	return choice;
+}
+
+function getRandomTileData(difficulty, isMain=false) {
+
+	var choices = DIFFICULTY_DATA[difficulty][isMain ? "main" : "choices"];
+
+	// get the random choice from the diff data
+	var choice = Utils.pickWeightedRandom(choices);
+
+	// compute the value using given limits and type
+	var value = undefined;
+	switch (choice.type) {
+		case "integer":
+			value = Utils.rand(...choice.limits);
+			break;
+		case "fraction":
+			// TODO do fraction stuff
+			break;
+		case "power":
+		case "exponent":
+			value = `${Utils.rand(...choice.baseLimits)} ^ ${choice.power || Utils.rand(...choice.powerLimits)}`;
+			break;
+		default:
+			value = NaN;
+	}
+
+	var operation = isMain ? "" : choice.operation;
+
+	return {
+		value,
+		valueString: operation+value,
+		operation,
+		condition: choice.condition,
+		maxCount: choice.maxCount
+	};
 }
